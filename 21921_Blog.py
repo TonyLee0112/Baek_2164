9833ec81a5697d352c1d9d53ae60430ff3fe6ec6
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2023-12-29 20:09:59 +0900

    Create 21921_Blog.py

---
+++ 21921_Blog.py
@@ -0,0 +1,17 @@
+N, X = map(int,input().split())
+Daily = list(map(int,input().split()))
+s = [Daily[0]]
+if Daily == [0] * N:
+    print("SAD")
+else :
+    for i in range(1,N) :
+        s.append(Daily[i]+s[i-1])
+    maxValue = 0
+    num = 1
+    for current_index in range(N-X) :
+        if maxValue < s[current_index+X]-s[current_index] :
+            maxValue = s[current_index+X]-s[current_index]
+        elif maxValue == s[current_index+X]-s[current_index] :
+            num += 1
+    print(maxValue)
+    print(num)
