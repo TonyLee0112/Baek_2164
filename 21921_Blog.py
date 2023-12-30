8a6e79b54ce51f7c3143e6ef0fa69353e8660a01
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2023-12-30 21:32:58 +0900

    Update 21921_Blog.py

--- 21921_Blog.py
+++ 21921_Blog.py
@@ -1,17 +1,24 @@
-N, X = map(int,input().split())
-Daily = list(map(int,input().split()))
+N, X = map(int, input().split())
+Daily = list(map(int, input().split()))
+
 s = [Daily[0]]
-if Daily == [0] * N:
+for i in range(1, N):
+    s.append(Daily[i] + s[i - 1])
+
+maxValue = s[X - 1]
+num = 1
+
+for current_index in range(1, N - X + 1):
+    current_sum = s[current_index + X - 1] - s[current_index - 1]
+
+    if maxValue < current_sum:
+        maxValue = current_sum
+        num = 1
+    elif maxValue == current_sum:
+        num += 1
+
+if maxValue == 0:
     print("SAD")
-else :
-    for i in range(1,N) :
-        s.append(Daily[i]+s[i-1])
-    maxValue = s[X-1]
-    num = 1
-    for current_index in range(N-X) :
-        if maxValue < s[current_index+X]-s[current_index] :
-            maxValue = s[current_index+X]-s[current_index]
-        elif maxValue == s[current_index+X]-s[current_index] :
-            num += 1
+else:
     print(maxValue)
     print(num)
