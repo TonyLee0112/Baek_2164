9b017ed15532b6f8e5cc5168ac5cafc92cac1b37
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2024-01-15 02:50:56 +0900

    Update 19637.py

--- 19637.py
+++ 19637.py
@@ -1,32 +1,34 @@
 import sys
-
 N, M = map(int, sys.stdin.readline().split())
 Title = []
 Power = []
 for i in range(N):
     title, figure = sys.stdin.readline().split()
     figure = int(figure)
-    if i == 0:
+    if i == 0 :
         Title.append(title)
         Power.append(figure)
-    elif not figure in Power:
-        Title.append(title)
-        Power.append(figure)
-def Binary_search(target, start, end):
+    if Power[len(Power)-1] == figure :
+        continue
+    Title.append(title)
+    Power.append(figure)
+def Binary_search(target):
+    start = 0
+    end = len(Power) - 1
     if target <= Power[start] :
         return Title[start]
-    elif end == start + 1 :
-        return Title[end]
     else :
-        meanVal = (start+end)//2
-        if target > Power[meanVal] :
-            return Binary_search(target,meanVal,end)
-        elif target == Power[meanVal] :
-            return Title[meanVal]
-        else :
-            return Binary_search(target,start,meanVal)
+        while end >= start :
+            mean_index = (start+end)//2
+            mean_power = Power[mean_index]
+            if end == start + 1 :
+                return Title[end]
+            elif target > mean_power :
+                start = mean_index
+            elif target < mean_power :
+                end = mean_index
+            elif target == mean_power :
+                return Title[mean_index]
 
-
-l = len(Power)
 for j in range(M) :
-    print(Binary_search(int(sys.stdin.readline()),0,l-1))
+    print(Binary_search(int(sys.stdin.readline())))
